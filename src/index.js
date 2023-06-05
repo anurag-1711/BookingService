abf5b7716a036fc4fa17fc975c8b52878d8e6c37
Anurag <anurag171199@gmail.com>
2023-06-05 16:08:12 +0530

    First commit with basic setup

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
+const db = require("./models/index");
+
+const setUpAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use("/api", apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT: ${PORT}`);
+
+        if (process.env.SYNC_DB) {
+            db.sequelize.sync({ alter: true });
+        }
+    });
+}
+
+setUpAndStartServer();
+
