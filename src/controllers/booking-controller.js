00011b4048fa2311de21695c56f51f775ab8afb9
Anurag <anurag171199@gmail.com>
2023-06-11 11:37:10 +0530

    Published demo message using RabbitMQ

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -14,8 +14,16 @@ class BookingController {
 
     async sendMessageToQueue(req, res) {
         const channel = await createChannel();
-        const data = { message: "Success" };
-        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        const payload = {
+            data: {
+                subject: "Notification from queue",
+                content: "Some queue will subscribe this",
+                recipientEmail: "anurag.gfg@gmail.com",
+                notificationTime: "2023-06-10T11:00:26",
+                service: "CREATE_TICKET"
+            }
+        };
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
         return res.status(200).json({
             message: "Successfully published the event"
         })
